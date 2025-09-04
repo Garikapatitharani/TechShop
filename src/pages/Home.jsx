3a82bb8715e424e27670bd2b4cef928c76f13b0e
Tharani Garikapati <tharanigarikapati@gmail.com>
2025-09-04 12:43:53 +0530

    Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,22 +3,36 @@ import { FaStar, FaArrowRight, FaTags, FaTruck, FaShieldAlt, FaRegCreditCard } f
 import productsData from "../contextAPI/ProductsData.js";
 import { addToCart } from "../rtk-store/cartSlice";
 import { useDispatch } from "react-redux";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 
 export default function Home() {
   const dispatch = useDispatch();
   const [added, setAdded] = useState(false);
- 
-  
+
 
   const featuredProducts = productsData.slice(0, 5);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-  const chunkSize = 4;
-  const slides = [];
-  for (let i = 0; i < featuredProducts.length; i += chunkSize) {
-    slides.push(featuredProducts.slice(i, i + chunkSize));
-  }
+  const handleNext = () => {
+    setCurrentIndex((prev) =>
+      prev === featuredProducts.length - 1 ? 0 : prev + 1
+    );
+  };
+
+  useEffect(() => {
+    const interval = setInterval(handleNext, 2000);
+    return () => clearInterval(interval);
+  }, []);
+
+
+  const getVisibleProducts = () => {
+    const visible = [];
+    for (let i = 0; i < 5; i++) {
+      visible.push(featuredProducts[(currentIndex + i) % featuredProducts.length]);
+    }
+    return visible;
+  };
 
 
 
@@ -47,11 +61,11 @@ export default function Home() {
                 <h6 className="mt-2 text-lg">₹9,999</h6>
                 <p className="line-through text-gray-400">₹12,499</p>
                 <Link to={`/product/7`}>
-                <button className="bg-red-500 px-4 py-2 text-white rounded mt-3">Shop now</button>
-             </Link>
+                  <button className="bg-red-500 px-4 py-2 text-white rounded mt-3">Shop now</button>
+                </Link>
               </div>
               <div className="image-section flex justify-center">
-                <img src="/products/sonyXb910n-1.png" className="h-80 px-10" alt="sony" />
+                <img src="/products/sonyXb910n-1.png" className="h-90 w-100 px-2 " alt="sony" />
               </div>
             </div>
           </div>
@@ -64,11 +78,11 @@ export default function Home() {
                 <h6 className="mt-2 text-lg">₹11,999</h6>
                 <p className="line-through text-gray-400">₹13,099</p>
                 <Link to={`/product/1`}>
-                <button className="bg-red-500 px-4 text-white py-2 mt-3 rounded">Shop now</button>
+                  <button className="bg-red-500 px-4 text-white py-2 mt-3 rounded">Shop now</button>
                 </Link>
               </div>
               <div className="image-section flex justify-center">
-                <img src="/products/jbl500bt-2.png" className="h-80 px-10" alt="JBL" />
+                <img src="/products/jbl500bt-2.png" className="h-90 w-100  px-2" alt="JBL" />
               </div>
             </div>
           </div>
@@ -81,11 +95,11 @@ export default function Home() {
                 <h6 className="mt-2 text-lg">₹1,099</h6>
                 <p className="line-through text-gray-400">₹2,199</p>
                 <Link to={`/product/3`}>
-                <button className="bg-red-500 px-4 py-2 mt-3 text-white rounded">Shop now</button>
-             </Link>
+                  <button className="bg-red-500 px-4 py-2 mt-3 text-white rounded">Shop now</button>
+                </Link>
               </div>
               <div className="image-section flex justify-center">
-                <img src="/products/boat131-3.png" className="h-80 px-10" alt="Airdopes" />
+                <img src="/products/boat131-3.png" className="h-90 w-100 px-2" alt="Airdopes" />
               </div>
             </div>
           </div>
@@ -104,52 +118,51 @@ export default function Home() {
 
 
 
-      {/* pricing */}
-      <div className="container bg-black">
-        <h2 className="text-center text-white mb-4">Featured Products</h2>
-        <div id="productCarousel" className="carousel slide" data-bs-ride="carousel">
-          <div className="carousel-inner">
-            {slides.map((group, index) => (
-              <div className={`carousel-item ${index === 0 ? "active" : ""}`} key={index}>
-                <div className="row justify-content-center">
-                  {group.map((product) => (
-                    <div className="col-md-3 text-white text-center" key={product.id}>
-                      <img src={product.heroImage} className="img-fluid mb-3" alt={product.title} style={{ height: "200px", objectFit: "contain" }} />
-                      <h6 className="text-white">{product.title}</h6>
-                      <p className="text-white fw-bold">{product.finalPrice}{""}
-                        <span className="text-gray-500 p-2 text-decoration-line-through">{product.originalPrice}</span>
-                      </p>
-                    </div>
-                  ))}
+      {/* Featured Products */}
+      <>
+        <h1 className="text-center bg-black text-white mb-0 pt-2">
+          Featured Products
+        </h1>
+
+        <div className="flex items-center justify-center gap-4 bg-black py-6">
+          {/* Products row */}
+          <div className="flex items-center gap-6">
+            {getVisibleProducts().map((product, index) => {
+              const middleIndex = Math.floor(5 / 2);
+              const isCenter = index === middleIndex;
+
+              return (
+                <div
+                  key={product.id}
+                  className={`transition-transform duration-500 ${isCenter ? "scale-110 z-10" : "scale-75 opacity-70"
+                    }`}
+                >
+                  <div className="p-4 bg-black text-white shadow-lg w-40 md:w-48">
+                    <h6 className="font-semibold text-sm md:text-base">
+                      {product.title}
+                    </h6>
+                    <Link
+                      to={`/product/${product.id}`}>
+                      <img
+                        src={product.images[0]}
+                        alt={product.title}
+                        className="w-full h-32 md:h-40 object-contain mb-2"
+                      />
+                    </Link>
+
+                    <p className="font-bold text-sm md:text-base text-white">
+                      ₹{product.finalPrice}{" "}
+                      <span className="line-through text-gray-500 text-xs">
+                        ₹{product.originalPrice}
+                      </span>
+                    </p>
+                  </div>
                 </div>
-              </div>
-            ))}
-          </div>
-
-          {/* Carousel Controls */}
-          <button className="carousel-control-prev" type="button" data-bs-target="#productCarousel" data-bs-slide="prev">
-            <span className="carousel-control-prev-icon"></span>
-          </button>
-          <button className="carousel-control-next" type="button" data-bs-target="#productCarousel" data-bs-slide="next">
-            <span className="carousel-control-next-icon"></span>
-          </button>
-
-          {/* Indicators */}
-          <div className="carousel-indicators">
-            {slides.map((_, index) => (
-              <button
-                key={index}
-                type="button"
-                data-bs-target="#productCarousel"
-                data-bs-slide-to={index}
-                className={index === 0 ? "active" : ""}
-              ></button>
-            ))}
+              );
+            })}
           </div>
         </div>
-      </div>
-
-
+      </>
 
 
       {/* Products */}
@@ -225,25 +238,25 @@ export default function Home() {
           </div>
         </div>
         <div className="flex items-center gap-3 p-3 bg-black">
-            <i class=" text-red-500"><FaShieldAlt size={28} /></i>
+          <i class=" text-red-500"><FaShieldAlt size={28} /></i>
           <div>
-          <h5 class=" text-white">Brand Warranty</h5>
-          <p class="text-gray-500">100% Original products</p>
+            <h5 class=" text-white">Brand Warranty</h5>
+            <p class="text-gray-500">100% Original products</p>
           </div>
         </div>
         <div className=" flex items-center gap-3 p-3 bg-black">
-            <i className="text-red-500 "><FaTags size={28} /></i>
+          <i className="text-red-500 "><FaTags size={28} /></i>
           <div>
-          <h5 className=" text-white">Exciting Deals</h5>
-          <p className="text-gray-500">On all prepaid orders</p>
-        </div>
+            <h5 className=" text-white">Exciting Deals</h5>
+            <p className="text-gray-500">On all prepaid orders</p>
+          </div>
         </div>
         <div className="flex items-center gap-3 p-3 bg-black">
-            <i className="text-red-500"><FaRegCreditCard size={28} /></i>
+          <i className="text-red-500"><FaRegCreditCard size={28} /></i>
           <div>
-          <h5 className="text-white">Secure Payments</h5>
-          <p className="text-gray-500">SSL/Secure certificate </p>
-        </div>
+            <h5 className="text-white">Secure Payments</h5>
+            <p className="text-gray-500">SSL/Secure certificate </p>
+          </div>
         </div>
       </div>
     </>
